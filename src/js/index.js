3c81d829c11b7368085cc8d6f1aba017617ac169
Paul Adu-Gyamfi <pjadu7@gmail.com>
2020-03-06 14:45:06 -0500

    added styling for double columns

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,12 @@ smoothscroll.polyfill();
 const wordArray = [
   `Tuesdays schedule I am so cool and awesome
   sss this is awesome. sdksd;lkdsl;kdsdkl;sdlk;sd
-  sdml;msd;mdssd;lm sdml;smldsdml;`
+  sdml;msd;mdssd;lm sdml;smldsdml;`, `Tuesdays schedule I am so cool and awesome
+  sss this is awesome. sdksd;lkdsl;kdsdkl;sdlk;sd
+  sdml;msd;mdssd;lm sdml;smldsdml;`, `Tuesdays schedule I am so cool and awesome
+  sss this is awesome. sdksd;lkdsl;kdsdkl;sdlk;sd
+  sdml;msd;mdssd;lm sdml;smldsdml;`,"Helllo njankjsannjk nasjknkjsna bkjasbkbjd",
+  'hi ajdksbjkbdsakjbsdbjksdbkasjb'
 ]
 
 startGroupingProcess(wordArray);
